619cdea894f5e18cfc416f03bfa4d09b03a53f68
Lucio B. Schiavoni Lafourcade <lucio.schiavoni@mec.local>
2024-10-16 12:51:15 -0300

    fix():producById

--- src/routes/product.routes.js
+++ src/routes/product.routes.js
@@ -1,6 +1,6 @@
 import { Router } from "express";
 import upload from "../middlewares/uploadFile.js";
-import { createProduct, getProducts } from "../controllers/product.controller.js";
+import { createProduct, getProductById, getProducts } from "../controllers/product.controller.js";
 
 
 const productRouter = Router();
@@ -8,7 +8,7 @@ const productRouter = Router();
 
 productRouter.post("/product" ,upload.single('product[img]'), createProduct)
 productRouter.get("/product", getProducts)
-productRouter.get("/product/:id", getProducts)
+productRouter.get("/product/:id", getProductById)
 
 
 export default productRouter;
